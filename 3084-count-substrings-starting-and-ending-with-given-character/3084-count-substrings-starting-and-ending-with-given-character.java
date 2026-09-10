8973160bc4bf580ae6170493db2825b51213d3d1
Knishka Kumari <knishka22k@gmail.com>
2026-09-10 22:53:06 +0530

    Time: 1 ms (100%), Space: 46.6 MB (40.75%) - LeetHub

---
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long countSubstrings(String s, char c) {
+        long ans = 0;
+        long count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == c){
+                count ++;
+                ans += count;
+            }
+        }
+        return ans;
+    }
+}
